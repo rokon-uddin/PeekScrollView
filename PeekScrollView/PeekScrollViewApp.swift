e10f1336b091b9f422b23162411b628633503aab
Mohammed Rokon Uddin <shoaib.rokon@gmail.com>
2024-09-30 14:43:48 +0600

    Initial Commit

---
+++ PeekScrollView/PeekScrollViewApp.swift
@@ -0,0 +1,17 @@
+//
+//  PeekScrollViewApp.swift
+//  PeekScrollView
+//
+//  Created by Mohammed Rokon Uddin on 9/30/24.
+//
+
+import SwiftUI
+
+@main
+struct PeekScrollViewApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
